9638832126d07dc22573d4a3182ae01383cec1ba
ifelsegrandmaster <patricechaula@gwerupoly.ac.zw>
2021-01-03 11:12:22 +0200

    Patch Sun Jan 3

--- marketing/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
-from .models import Subscriber
-# Create your views here.
-# I think it's now time to beautify the thing and finish it op
