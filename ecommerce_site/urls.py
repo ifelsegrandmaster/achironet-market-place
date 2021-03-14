84056689a74b0bfd6146234cb421449a5f0fef5c
ifelsegrandmaster <patricechaula@gwerupoly.ac.zw>
2021-03-14 13:56:36 +0200

    Modified urls

--- ecommerce_site/urls.py
+++ ecommerce_site/urls.py
@@ -57,6 +57,4 @@ urlpatterns = [
          name='django.contrib.sitemaps.views.sitemap')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+
